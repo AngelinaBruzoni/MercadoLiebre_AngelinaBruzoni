8157b86b1a1c26d5f407ade2792c9cea1e3d9593
AngelinaBruzoni <angelinabruzoni2050@gmail.com>
2023-11-26 03:35:48 -0300

    Primera versión de agregando estilos al home.html

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 //Importamos la librería Express
 const express = require('express');
+
 //Ejecutamos la función express, obteniendo todas las funcionalidades. app es un objeto
 //literal con propiedades y funcionalidades de EXPRESS.
 const app = express();
+
 //Importamos el modulo nativo PATH
 const path = require('path');
 
@@ -13,7 +15,7 @@ app.listen(3030, ()=>{
 
 //Configuramos el sistema de ruteo para dar respuesta a las peticiones del cliente
 app.get('/',function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));// (.) indica donde estas actualmente.
 });
 
 //Seteamos la ruta a la carpeta de recursos estáticos public
